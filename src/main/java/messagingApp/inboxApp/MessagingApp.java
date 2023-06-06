6eb1eb0e5e7c6b2623d23425e7f71469e35b4d29
akumbhare47 <akumbhare47@gmail.com>
2023-06-06 23:01:26 +0530

    working github handshake

--- src/main/java/messagingApp/inboxApp/MessagingApp.java
+++ src/main/java/messagingApp/inboxApp/MessagingApp.java
@@ -1,4 +1,4 @@
-package io.javabrains;
+package messagingApp.inboxApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringGitHubLoginApplication {
+public class MessagingApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGitHubLoginApplication.class, args);
+		SpringApplication.run(MessagingApp.class, args);
 	}
 
 	@RequestMapping("/user")
--- src/main/java/messagingApp/inboxApp/SecurityAdapter.java
+++ src/main/java/messagingApp/inboxApp/SecurityAdapter.java
@@ -1,4 +1,4 @@
-package io.javabrains;
+package messagingApp.inboxApp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
