f428378575d2c70f2501349854d2352960c86dd7
akumbhare47 <akumbhare47@gmail.com>
2023-06-06 21:35:34 +0530

    some changes

--- MessagingApp/src/main/java/Messagingapp/demo/MessagingApp.java
+++ MessagingApp/src/main/java/Messagingapp/demo/MessagingApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -15,7 +15,7 @@ public class MessagingApp {
 		SpringApplication.run(MessagingApp.class, args);
 	}
 
-	@RequestMapping("/user")
+	@GetMapping("/user")
 	public String user(@AuthenticationPrincipal OAuth2User principal) {
 		System.out.println(principal);
 		return principal.getAttribute("name");
