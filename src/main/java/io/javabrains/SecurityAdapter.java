8aa301bccf7e47b808d510752876e7412ec7896a
akumbhare47 <akumbhare47@gmail.com>
2023-06-06 22:49:56 +0530

    new repo

--- src/main/java/io/javabrains/SecurityAdapter.java
+++ src/main/java/io/javabrains/SecurityAdapter.java
@@ -1,19 +1,18 @@
-package Messagingapp.demo;
+package io.javabrains;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
-public class SecurityAdapter {
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
 
-
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+	protected void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
 		http
 			.authorizeRequests(a -> a
@@ -30,7 +29,6 @@ public class SecurityAdapter {
 				.logoutSuccessUrl("/").permitAll()
 			)
 			.oauth2Login();
-        return http.build();
 		// @formatter:on
     }
     
